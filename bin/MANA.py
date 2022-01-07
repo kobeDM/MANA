8901560f9f133c324da84920c9626a645884e180
msgc <Kentaro Miuchi>
2022-01-07 14:02:14 +0900

    first light

--- bin/MANA.py
+++ bin/MANA.py
@@ -10,7 +10,8 @@ import json
 
 from subprocess import PIPE
 #from subprocess import STDOUT
-MANAPATH="/home/msgc/miraclue/MANA/bin/"
+HOME=os.environ["HOME"]
+MANAPATH=HOME+"/miraclue/MANA/bin/"
 MANAVIEW="MANA_view"
 MANASPEC="MANA_spec"
 #VIEW_SKEL="View_skel.cc"
@@ -19,10 +20,38 @@ CONFIG="MADA_config.json"
 print("**MANA.py**")
 print("**in Micacle argon ANAlysis (http://github.com/kobeDM/MADA)**")
 print("**2021 Sep by K. Miuchi**")
-files=['GBIP_016_0000.raw','GBIP_024_0000.raw']
+files=[]
+dirs=[]
 
-for file in files:
-    cmd='xterm -e '+MANAPATH+MANASPEC+" "+file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s",help="start file ID",default=0,type=int)
+parser.add_argument("-e",help="end file ID",default=1000,type=int)
+args = parser.parse_args( )
+start=args.s
+end=args.e
+
+fileid=start
+
+#load config file
+config_open= open(CONFIG,'r')
+config_load = json.load(config_open)
+#activeIP=[]
+for x in config_load['gigaIwaki']:
+#    files.append(x+"_0000.mada")
+    files.append(x+"_"+(str(fileid).zfill(4))+".mada")
+    dirs.append(os.getcwd());
+
+#    if config_load['gigaIwaki'][x]['active']==1:
+#        activeIP.append(config_load['gigaIwaki'][x]['IP'])
+#        print(config_load['gigaIwaki'][x]['IP'])
+#load config file ends.
+
+print(files)
+
+for i in range(len(files)):
+#for file in files:
+    cmd='xterm -geometry 50x5+300+'+str(i*100)+' -e '+MANAPATH+MANASPEC+" "+files[i]+" "+dirs[i]
     #file="GBIP_016_0000.raw"
     #cmd=MANAPATH+MANASPEC+" "+file
     print(cmd)
